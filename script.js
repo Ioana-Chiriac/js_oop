bd113692f9a16d0cb2125d3eafb2be7782375305
Ioana-Chiriac <ioana.chiriac1014@gmail.com>
2024-03-20 18:56:55 +0200

    add besic class and exemples

--- script.js
+++ script.js
@@ -5,3 +5,18 @@ class Product {
         this.stockQuantity = stockQuantity;
     }
 }
+
+const product1 = new Product(`Bicicleta`, 750, 1000);
+console.log(product1);
+
+const product2 = {
+    productName: `Bicicleta`,
+    price: 750,
+    stockQuantity: 1000,
+}
+
+console.log(product2);
+
+const product3 = new Product(`Leptop`, 2500, 300);
+console.log(product3);
+
