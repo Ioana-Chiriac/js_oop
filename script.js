9c3efcd721374eba7426db1664d137949b267d0f
Ioana-Chiriac <ioana.chiriac1014@gmail.com>
2024-03-20 18:44:21 +0200

    make the base

---
+++ script.js
@@ -0,0 +1,7 @@
+class Product {
+    constructor(productName, price, stockQuantity) {
+        this.productName = productName;
+        this.price = price;
+        this.stockQuantity = stockQuantity;
+    }
+}
